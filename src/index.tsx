0f404bcac261afd9ecf8ce6fa7c09d213ad75772
vladmalevich14 <vladmalevich1431@gmail.com>
2024-05-20 15:48:54 +0300

    Replace BrowserRouter with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "app/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter basename={'/art-for-introvert'}>
+    <HashRouter>
     <Provider store={store}>
             <App/>
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
